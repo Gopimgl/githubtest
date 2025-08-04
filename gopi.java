e192a511d3152e82e9a62a4d10c876e7a709c89f
Gopimgl <gopimaniganga@gmail.com>
2025-08-04 22:52:51 +0530

    modifying

--- gopi.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class gopi {
-    public static void main(String[] args){
-        System.out.println(args);
-        main(new String[]{"mgl","main"});
-
-    }
-    
-}
---
+++ gopimgl.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class gopimgl {
+    int a=0;
+    public static void main(String[] args){
+        System.out.println(args);
+        gopimgl g=new gopimgl();
+       
+        if(g.a==0){
+   g.a++;
+        main(new String[]{"mgl","main"});
+        }
+    }
+}
